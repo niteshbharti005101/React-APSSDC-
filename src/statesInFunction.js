bc6e5ac44c758100b4b3241d8d55d67e35ee3234
Nitesh <niteshbharti005101@gmail.com>
2020-07-20 10:53:34 +0530

    Update statesInFunction.js

--- src/statesInFunction.js
+++ src/statesInFunction.js
@@ -1,14 +1,14 @@
 import React,{useState} from 'react';
 
 let StatesInFunction=()=>{
-    const [count,setCount]=useState("Bye");
+    const [count,setCount]=useState();
     return(
         <div>
             <h2> States In Function Component</h2>
             <h2> {count}</h2>
             <h2> {typeof(count)}</h2>
             <h2 onMouseOver={()=>setCount("Hi")}
-                onMouseLeave={()=>setCount("Bye")}> {count}</h2>
+                onMouseLeave={()=>setCount("Bye")}> {typeof(count)}</h2>
 
         </div>
     )
